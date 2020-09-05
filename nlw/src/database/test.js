7662d532288eb92b35d63ceb01550f7b8e313516
Felipe Rangel Ribeiro <lipefortaleza2008@hotmail.com>
2020-09-05 02:18:18 -0300

    Conexão com o banco de dados e inserção de dados

--- nlw/src/database/test.js
+++ nlw/src/database/test.js
@@ -1,8 +1,9 @@
 const db = require("./db")
+const createProffy = require('./createProffy')
 
-db.then((db) => {
+db.then(async (db) => {
     //inserir dados
-    proffy = {
+    proffyValue = {
         name: "Diego Fernandes",
         avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
         whatsapp: "89987654534",
@@ -10,12 +11,12 @@ db.then((db) => {
     }
 
     classValue = {
-        subject: "Química",
+        subject: 1,
         cost: "20",
         // o proffy id virá pelo banco de dados
     }
 
-    classSchedule = [
+    classScheduleValues = [
         {
             //class_id virá pelo banco de dados, após cadastrarmos a class
             weekday: 1,
@@ -24,10 +25,40 @@ db.then((db) => {
 
         },
         {
-            weekday: 1,
+            weekday: 0,
             time_from: 520,
             time_to: 1220
 
         },
     ]
+    //await createProffy(db, {proffyValue, classValue, classScheduleValues})
+    
+    //Consultar dados
+        //todos os proffys
+            const selectedProffys = await db.all("SELECT * FROM proffys")
+            //console.log(selectedProffys)
+
+    // Consultar as classes de um determinado professor
+    // e trazer junto os dados do professor(chave estrangeira)
+    const selectedClassesAndProffys = await db.all(`
+        SELECT classes.*, proffys.*
+        FROM proffys
+        JOIN classes ON (classes.proffy_id = proffys.id)
+        WHERE classes.proffy_id = 1;    
+    `)
+        //console.log(selectedClassesAndProffys)
+
+        // o horário que a pessoa trabalha, por exemplo é das 8h - 18h
+        // o horário do time_from(8h) precisa ser menor ou igual ao horário solicitado
+        //o time_to precisa ser acima
+
+        const selectedClassesSchedules = await db.all(`
+            SELECT class_schedule.*
+            FROM class_schedule
+            WHERE class_schedule.class_id = "1"
+            AND class_schedule.weekday = 0
+            AND class_schedule.time_from <= "520"
+            AND class_schedule.time_to > "520"
+        `)
+        console.log(selectedClassesSchedules)
 })
